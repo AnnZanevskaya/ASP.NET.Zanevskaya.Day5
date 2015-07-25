a8f951b17f9122844b90aada06543ac1d15b6d1f
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-07-25 22:44:28 +0300

    book methods

---
+++ ASP.NET.Zanevskaya.Day5/Task1.LibraryN/BookListService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Logger;
+
+
+namespace Task1.LibraryN
+{
+    public class BookListService
+    {
+        private ILogger logger = new NLogAdapter();
+        private IBookRepository<Book> repository;
+        private List<Book> books = new List<Book>();
+
+        public BookListService(IBookRepository<Book> repository)
+        {
+            if (ReferenceEquals(repository, null))
+            {
+                Exception e = new ArgumentNullException("repository is invalid(null)");
+                logger.Info(e.Message);
+                logger.Error(e.StackTrace);
+                throw e;
+            } 
+            this.repository = repository;
+        }
+        public void AddBook(Book book)
+        {
+            if (ReferenceEquals(book, null))
+            {
+                Exception e = new ArgumentNullException("book is invalid(null)");
+                logger.Info(e.Message);
+                logger.Error(e.StackTrace);
+                throw e;
+            }
+            if (BookExistence(book))
+            {
+                Exception e = new ArgumentNullException("book already in repository");
+                logger.Info(e.Message);
+                logger.Error(e.StackTrace);
+                throw e;
+            }
+            books.Add(book);
+            repository.SaveBooks(books);
+            logger.Info(String.Format("add new book {0}", book));
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (ReferenceEquals(book, null))
+            {
+                Exception e = new ArgumentNullException("book is invalid(null)");
+                logger.Info(e.Message);
+                logger.Error(e.StackTrace);
+                throw e;
+            }
+            if (!BookExistence(book))
+            {
+                Exception e = new ArgumentNullException("book doesn't exist");
+                logger.Info(e.Message);
+                logger.Error(e.StackTrace);
+                throw e;
+            }
+            books.Remove(book);
+            repository.SaveBooks(books);
+            logger.Info(String.Format("remove book {0}", book));
+        }
+        public IEnumerable<Book> GetBooks()
+        {
+            return repository.LoadBooks();
+        }
+        public void SortBooksByTag(IComparer<Book> comparer)
+        {
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            books = repository.LoadBooks().ToList();
+            books.Sort(comparer);
+            repository.SaveBooks(books);           
+        }
+        private bool BookExistence(Book book)
+        {
+            Book existBook = books.FirstOrDefault(x => x.Equals(book));
+            if (ReferenceEquals(existBook, null)) return false;
+            return true;
+
+        }
+    }
+}
---
+++ ASP.NET.Zanevskaya.Day5/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.LibraryN;
+using Task1.Logger;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryBookRepository rep = new BinaryBookRepository("list.bin");
+            BookListService s = new BookListService(rep);
+            
+            s.AddBook(new Book("Author1", "Book1", "Genre1", 24));
+            s.AddBook(new Book("Author2", "Book2", "Genre2", 27));
+            s.AddBook(new Book("Author3", "Book1", "Genre2", 24));
+            s.RemoveBook(new Book("Author3", "Book1", "Genre2", 24));
+            s.RemoveBook(new Book("Author3", "Book1", "Genre2", 24));
+            Console.WriteLine("Books:");
+            PrintBooks(rep.LoadBooks());
+        }
+            private static void PrintBooks(IEnumerable<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        
+    }
+}
